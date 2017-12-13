b8a5057a44356df69f4bf4c54c49effbadc9205d
“Tea <teateearu@gmail.com>
2017-12-13 14:52:37 +0100

    Change schemas again

--- models/batch.js
+++ models/batch.js
@@ -1,22 +1,10 @@
 const mongoose = require('../config/database')
 const { Schema } = mongoose
 
-const evaluationSchema = new Schema({
-  date: { type: Date, default: Date.now },
-  color: { type: String, default: 'green', required: true},
-});
-
-const studentSchema = new Schema({
-  name: { type: String, required: true },
-  photo: { type: String, required: true },
-  evaluations: [evaluationSchema],
-});
-
 const batchSchema = new Schema({
   batchNumber: { type: String, required: true },
   startDate: { type: Date, default: Date.now },
-  endDate: { type: Date, default: Date.now },
-  students: [studentSchema],
+  endDate: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('batches', batchSchema)
