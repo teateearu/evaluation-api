057b31d9a05fd6b0bb4c420b574f0197561dbba5
“Tea <teateearu@gmail.com>
2017-12-16 20:32:50 +0100

    Add color to student schema

--- models/student.js
+++ models/student.js
@@ -6,6 +6,7 @@ const studentSchema = new Schema({
   name: { type: String, required: true },
   photo: { type: String, required: true },
   batch_id: { type: Schema.Types.ObjectId, ref: 'batches' },
+  color: { type: String, default: 'green' }
 });
 
 module.exports = mongoose.model('students', studentSchema)
